77bae40d7aad970e4955b98ff167b32a05de6aeb
SpartakMartoyan <162327607+SpartakMartoyan@users.noreply.github.com>
2025-11-30 15:27:57 +0400

    Update remote_data_source.dart
    Refactor getTopHeadlines method to include category and query parameters for API requests.

--- lib/data/datasources/remote_data_source.dart
+++ lib/data/datasources/remote_data_source.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import '../models/article_model.dart';
 
 abstract class RemoteDataSource {
-  // We added category and query here
   Future<List<ArticleModel>> getTopHeadlines({String? category, String? query});
 }
 
@@ -12,21 +11,17 @@ class RemoteDataSourceImpl implements RemoteDataSource {
 
   RemoteDataSourceImpl({required this.client});
 
-  // TODO: Put your API Key here
   final String apiKey = '7b703952653347b48b8fabb49b8c5263';
   final String baseUrl = 'https://newsapi.org/v2';
 
   @override
   Future<List<ArticleModel>> getTopHeadlines({String? category, String? query}) async {
-    // 1. Base URL
     String url = '$baseUrl/top-headlines?country=us&apiKey=$apiKey';
 
-    // 2. Add Category if it exists
     if (category != null && category.isNotEmpty) {
       url += '&category=$category';
     }
 
-    // 3. Add Search Query if it exists
     if (query != null && query.isNotEmpty) {
       url += '&q=$query';
     }
